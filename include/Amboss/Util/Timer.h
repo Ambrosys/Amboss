a2807744ba4c47afa78e543baaf326d8c85d00cb
Karsten Ahnert <karsten.ahnert@gmx.de>
2013-04-08 09:01:57 +0200

    added Timer

---
+++ include/Amboss/Util/Timer.h
@@ -0,0 +1,48 @@
+/*
+ * Timer.h
+ * Date: 2013-04-06
+ * Author: Karsten Ahnert (karsten.ahnert@gmx.de)
+ */
+
+#ifndef AMBOSS_TIMER_H_INCLUDED
+#define AMBOSS_TIMER_H_INCLUDED
+
+#include <chrono>
+
+
+namespace Amboss {
+namespace Util {
+
+
+class Timer
+{
+    typedef std::chrono::high_resolution_clock clock_type;
+    clock_type::time_point m_start_time;
+
+    template< class T >
+    static inline double get_seconds( T t )
+    {
+        return double( std::chrono::duration_cast< std::chrono::milliseconds >( t ).count() ) / 1000.0;
+    }
+
+public:
+
+    Timer( void ) : m_start_time( clock_type::now() ) { }
+
+    inline double seconds( void ) const
+    {
+        return get_seconds( clock_type::now() - m_start_time );
+    }
+
+    inline void restart( void )
+    {
+        m_start_time = clock_type::now();
+    }
+};
+
+
+} // namespace Util
+} // namespace Amboss
+
+
+#endif // AMBOSS_TIMER_H_INCLUDED
